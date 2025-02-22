64fc72a7f25093b6a4741006f1d4d4c87dcad13e
AnitaJonker <anitaroneljonker@gmail.com>
2025-02-22 11:57:24 +0200

    Update templates , views and urls

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,17 @@ from django.http import HttpResponse
 
 def index(request):
     context = {
-        "todo": "Hi Todo App",
+        "title": "To Do",
     }
-    return render(request, "main.html", context)
+    return render(request, "cards/to_do_card.html" , context)
+def hero(request):
+    context = {
+        "title": "To Do",
+    }
+    return render(request, "heroes/hero.html")
+
+def main(request):
+    context = {
+        "title": "To Do",
+    }
+    return render(request, "main.html")

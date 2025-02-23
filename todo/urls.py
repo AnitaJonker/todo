b0246929212af52f7216ac693cfa3972a8d8acfc
AnitaJonker <anitaroneljonker@gmail.com>
2025-02-23 14:19:04 +0200

    add urls for edit,save,cancel

--- todo/urls.py
+++ todo/urls.py
@@ -10,6 +10,9 @@ urlpatterns = [
     path("tasks/<int:list_num>", views.new_tasks, name="tasks"),
     path("tasks/<int:list_num>/new_tasks", views.new_tasks, name="new_task"),
     path("update-task/<int:task_id>/", views.update_task_status, name="update_task_status"),
+    # path("update-name/<int:task_id>/<str:updated_name>", views.update_task_name, name="update_task_name"),
+    path("edit_task/<int:task_id>", views.edit_task, name="edit_task"),
+    path("cancel_task/<int:task_id>", views.cancel_task, name="cancel_task"),
+    path("save_task/<int:task_id>", views.save_task, name="save_task"),
     path("delete-task/<int:task_id>/", views.delete_task, name="delete_task"),
-    
 ]

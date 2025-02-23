272beff36ad3dbcfc079734e690187d1251a9122
AnitaJonker <anitaroneljonker@gmail.com>
2025-02-23 21:28:10 +0200

    include django authentication urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,9 +18,8 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-   
-    path("", include("todo.urls")),
     path('admin/', admin.site.urls),
+    path("", include("todo.urls")),
     path("members/", include("django.contrib.auth.urls")),
     path("members/", include("members.urls")),
 ]

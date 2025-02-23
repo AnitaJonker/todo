789ea9afbc084c14d84578ae65a90d30ecbbea2a
AnitaJonker <anitaroneljonker@gmail.com>
2025-02-23 21:31:41 +0200

    update urls and remove un-used urls

--- todo/urls.py
+++ todo/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("hero", views.hero, name="hero"),
-    path("main", views.main, name="main"),
+    path("", views.home, name="home"),
+    path("index", views.index, name="index"),
+    path("search/<str:search>", views.search, name="search"),
     path("create/list", views.create_list_form, name="create_list_form"),
     path("tasks/<int:list_num>", views.new_tasks, name="tasks"),
     path("tasks/<int:list_num>/new_tasks", views.new_tasks, name="new_task"),

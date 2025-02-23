8a8a0196eed9a55d81685cb2622c51e3b7035b20
AnitaJonker <anitaroneljonker@gmail.com>
2025-02-23 21:27:09 +0200

    update authentication urls

--- members/urls.py
+++ members/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 from . import views
 
     
-urlpatterns = [path("login_user", views.login_user, name="login_user")]
+urlpatterns = [path("login_user", views.login_user, name="login_user"),
+               path("register", views.registerPage, name="register"),
+               path("logout", views.logout_view, name="logout")]

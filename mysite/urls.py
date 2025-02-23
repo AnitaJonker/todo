5091a314b684935fc72656d07c5dba25d1e03e56
AnitaJonker <anitaroneljonker@gmail.com>
2025-02-23 15:43:06 +0200

    set up members url, make use of django auth packages

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+   
     path("", include("todo.urls")),
     path('admin/', admin.site.urls),
+    path("members/", include("django.contrib.auth.urls")),
+    path("members/", include("members.urls")),
 ]
